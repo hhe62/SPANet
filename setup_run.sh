075aa887ab066272a2d21f69389af93693d19e4a
He He <he.he@cern.ch>
2026-06-17 17:11:58 -0500

    Alternative option file for testing; add useful commands

---
+++ setup_run.sh
@@ -0,0 +1,3 @@
+bash /workspace/Miniconda3-latest-Linux-x86_64.sh
+git clone https://github.com/hhe62/SPANet.git
+conda env create -p ./environment --file environment_cuda118.yaml
---
+++ test_predict.sh
@@ -0,0 +1,1 @@
+python -m spanet.predict ./spanet_output/version_0/ ./spanet_ttHH_testing_output.h5 -tf /workspace/h5/DL_random.h5 --gpu
