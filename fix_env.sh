892fd8ef648aae4e876907e7732f811d44d96ae1
He He <he.he@cern.ch>
2026-06-24 06:16:14 -0500

    Update recipes

--- fix_env.sh
+++ fix_env.sh
@@ -1,2 +1,3 @@
 pip install "rich<14"
 pip install "setuptools<81"
+pip install h5py
--- setup_run.sh
+++ setup_run.sh
@@ -1,3 +1,4 @@
 bash /workspace/Miniconda3-latest-Linux-x86_64.sh
 git clone https://github.com/hhe62/SPANet.git
 conda env create -p ./environment --file environment_cuda118.yaml
+git clone https://github.com/hhe62/ttHH_TestStage.git
